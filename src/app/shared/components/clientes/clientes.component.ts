91486ece24a16a1b3c57a685c09affb3c1796bfd
oscar <oscar.rojas.p@uni.pe>
2024-08-14 08:09:08 -0500

    modal en componente cliente

--- src/app/shared/components/clientes/clientes.component.ts
+++ src/app/shared/components/clientes/clientes.component.ts
@@ -1,23 +1,36 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-clientes',
   standalone: true,
   imports: [
     CommonModule,
-
+    ModalComponent
   ],
   templateUrl: './clientes.component.html',
   styleUrl: './clientes.component.css'
 })
 export default class ClientesComponent {
-  items: Array<null>;
-  valor: string;
+  items = Array(100).fill(null);
+  isOpenRegistro: boolean = false;
+  isOpenEliminar: boolean = false;
+  tituloModalRegistro: string = '';
 
-
-  constructor() {
-    this.items = Array.from({length:100});
-    this.valor = 'test';
+  openModalRegistro(titulo: string){
+    this.isOpenRegistro = true;
+    this.tituloModalRegistro = titulo;
   }
+  closeModalRegistro(){
+    this.isOpenRegistro = false;
+  }
+
+  openModalEliminar(){
+    this.isOpenEliminar = true;
+  }
+  closeModalEliminar(){
+    this.isOpenEliminar = false;
+  }
+
 }
